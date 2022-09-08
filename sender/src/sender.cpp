3ded5df972e544bb78fab02e19e3a2aafddb933b
gautam-sh <gtmsharma70@gmail.com>
2022-09-08 14:44:28 +0530

    try to reduce CCN

--- sender/src/sender.cpp
+++ sender/src/sender.cpp
@@ -5,31 +5,31 @@
 int main(int argc, char *argv[])
 {
 
-    if (argc > 1 && std::string(argv[1]) == "test")
-        test_data_generation();
-
-    else
+    if (argc > 1 && std::string(argv[1])=="test")
     {
-        GenericSensor temperature_sensor;
-        double min_temperature{20};
-        double max_temperature{80};
+        test_data_generation();
+        return 0;
+    }
 
-        for (int i = 0; i < 50; ++i)
-        {
-            temperature_sensor.generateData(min_temperature, max_temperature);
-            std::cout << "Temperature = ";
-            temperature_sensor.sendData();
-        }
+    GenericSensor temperature_sensor;
+    double min_temperature{20};
+    double max_temperature{80};
 
-        GenericSensor SoC_sensor;
-        double min_soc{0};
-        double max_soc{45};
+    for (int i = 0; i < 50; ++i)
+    {
+        temperature_sensor.generateData(min_temperature, max_temperature);
+        std::cout << "Temperature = ";
+        temperature_sensor.sendData();
+    }
 
-        for (int i = 0; i < 50; ++i)
-        {
-            SoC_sensor.generateData(min_soc, max_soc);
-            std::cout << "SoC = ";
-            SoC_sensor.sendData();
-        }
+    GenericSensor SoC_sensor;
+    double min_soc{0};
+    double max_soc{45};
+
+    for (int i = 0; i < 50; ++i)
+    {
+        SoC_sensor.generateData(min_soc, max_soc);
+        std::cout << "SoC = ";
+        SoC_sensor.sendData();
     }
 }
