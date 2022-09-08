25716a378600f2baa9760da6670c9dff443430ca
gautam-sh <gtmsharma70@gmail.com>
2022-09-08 14:17:00 +0530

    remove multiple main files

--- sender/src/sender.cpp
+++ sender/src/sender.cpp
@@ -1,28 +1,35 @@
 #include "generic_sensor.hpp"
-
+#include "../tests/test_sender.hpp"
 #include <iostream>
 
-int main()
+int main(int argc, char *argv[])
 {
-    GenericSensor temperature_sensor;
-    double min_temperature{20};
-    double max_temperature{80};
 
-    for (int i = 0; i < 50; ++i)
+    if (std::string(argv[1]) == "test")
+        test_data_generation();
+
+    else
     {
-        temperature_sensor.generateData(min_temperature, max_temperature);
-        std::cout << "Temperature = ";
-        temperature_sensor.sendData();
-    }
+        GenericSensor temperature_sensor;
+        double min_temperature{20};
+        double max_temperature{80};
 
-    GenericSensor SoC_sensor;
-    double min_soc{0};
-    double max_soc{45};
+        for (int i = 0; i < 50; ++i)
+        {
+            temperature_sensor.generateData(min_temperature, max_temperature);
+            std::cout << "Temperature = ";
+            temperature_sensor.sendData();
+        }
 
-    for (int i = 0; i < 50; ++i)
-    {
-        SoC_sensor.generateData(min_soc, max_soc);
-        std::cout << "SoC = ";
-        SoC_sensor.sendData();
+        GenericSensor SoC_sensor;
+        double min_soc{0};
+        double max_soc{45};
+
+        for (int i = 0; i < 50; ++i)
+        {
+            SoC_sensor.generateData(min_soc, max_soc);
+            std::cout << "SoC = ";
+            SoC_sensor.sendData();
+        }
     }
 }
