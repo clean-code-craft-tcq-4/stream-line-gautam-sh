8e631f549468687932f664631936f6dff5760b3b
gautam-sh <gtmsharma70@gmail.com>
2022-09-08 11:48:10 +0530

    add implementation + fix git build

---
+++ sender/src/sender.cpp
@@ -0,0 +1,28 @@
+#include "generic_sensor.hpp"
+
+#include <iostream>
+
+int main()
+{
+    GenericSensor temperature_sensor;
+    double min_temperature{20};
+    double max_temperature{80};
+
+    for (int i = 0; i < 50; ++i)
+    {
+        temperature_sensor.generateData(min_temperature, max_temperature);
+        std::cout << "Temperature = ";
+        temperature_sensor.sendData();
+    }
+
+    GenericSensor SoC_sensor;
+    double min_soc{0};
+    double max_soc{45};
+
+    for (int i = 0; i < 50; ++i)
+    {
+        SoC_sensor.generateData(min_soc, max_soc);
+        std::cout << "SoC = ";
+        SoC_sensor.sendData();
+    }
+}
--- sender/tests/test_sender.cpp
+++ sender/tests/test_sender.cpp
@@ -1,8 +1,11 @@
+#include "generic_sensor.hpp"
 #include <assert.h>
 
 int main()
 {
     // Test if sensor data generated is within default range of 0 and 100
-    double test_sensor_data = test_sensor.generateData();
+    GenericSensor test_sensor;
+    test_sensor.generateData();
+    double test_sensor_data = test_sensor.getData();
     assert(test_sensor_data > 0 && test_sensor_data < 100);
 }
