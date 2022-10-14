4724ecacc75d475b6190ca692d53b00e2f656652
ygn5cob <38490766+ygn5cob@users.noreply.github.com>
2022-10-14 15:46:46 +0530

    Update receiver.cpp

--- receiver/src/receiver.cpp
+++ receiver/src/receiver.cpp
@@ -1,4 +1,17 @@
 #include <iostream>
+#include <queue>
+
+double calculate_average(std::queue<double>& input_queue)
+{
+   double queue_sum = 0;
+   double queue_size = input_queue.size();
+   while(input_queue.size())
+   {
+      std::cout << input_queue.front() << std::endl;
+      queue_sum += input_queue.front();
+   }
+   return queue_sum/queue_size;
+}
 
 int main(int argc, char *argv[])
 {
@@ -6,6 +19,8 @@ int main(int argc, char *argv[])
    double min_value = 0;
    double max_value = 0;
    bool is_first_value = true;
+   std::queue<double> average_queue;
+   const int DEFAULT_QUEUE_SIZE = 5;
    while (std::cin >> current_value)
    {
       std::cout << current_value << std::endl; //this log could be removed later if not needed
@@ -19,8 +34,15 @@ int main(int argc, char *argv[])
          min_value = current_value;
       if(max_value < current_value)
          max_value = current_value;
+      if(average_queue.size() >= DEFAULT_QUEUE_SIZE)
+      {
+         average_queue.pop();
+      }
+      average_queue.push(current_value);
    }
    std::cout << "Minimum value: " << min_value << std::endl;
    std::cout << "Maximum value: " << max_value << std::endl;
+   std::cout << "Maximum value: " << max_value << std::endl;
+   std::cout << "Simple Moving Average of last 5 values: " << calculate_average(average_queue) << std::endl;
    return 0;
 }
