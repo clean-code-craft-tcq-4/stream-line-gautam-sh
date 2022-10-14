fc5e0e8fb7b9991f3d94c5ac70638f158825b11e
ygn5cob <38490766+ygn5cob@users.noreply.github.com>
2022-10-14 16:56:44 +0530

    Update receiver.cpp

--- receiver/src/receiver.cpp
+++ receiver/src/receiver.cpp
@@ -1,49 +1,20 @@
 #include <iostream>
 #include <queue>
-
-double calculate_average(std::queue<double>& input_queue)
-{
-   double queue_sum = 0;
-   double queue_size = input_queue.size();
-   while(input_queue.size())
-   {
-      std::cout << input_queue.front() << std::endl;
-      queue_sum += input_queue.front();
-      input_queue.pop();
-   }
-   return queue_sum/queue_size;
-}
+#include "statistics.hpp"
 
 int main(int argc, char *argv[])
 {
-   double current_value = 0;
-   double min_value = 0;
-   double max_value = 0;
-   bool is_first_value = true;
-   std::queue<double> average_queue;
-   const int DEFAULT_QUEUE_SIZE = 5;
+   double current_value = 0;   
+   Statistics statistics;
    while (std::cin >> current_value)
    {
       std::cout << current_value << std::endl; //this log could be removed later if not needed
-      if(is_first_value)
-      {
-         min_value = current_value;
-         max_value = current_value;
-         is_first_value = false;
-      }
-      if(min_value > current_value)
-         min_value = current_value;
-      if(max_value < current_value)
-         max_value = current_value;
-      if(average_queue.size() >= DEFAULT_QUEUE_SIZE)
-      {
-         average_queue.pop();
-      }
-      average_queue.push(current_value);
+      statistics.checkMinValue(current_value);
+      statistics.checkMaxValue(current_value);
+      statistics.addAvrValue(current_value);
    }
-   std::cout << "Minimum value: " << min_value << std::endl;
-   std::cout << "Maximum value: " << max_value << std::endl;
-   std::cout << "Maximum value: " << max_value << std::endl;
-   std::cout << "Simple Moving Average of last 5 values: " << calculate_average(average_queue) << std::endl;
+   std::cout << "Minimum value: " << statistics.getMinValue() << std::endl;
+   std::cout << "Maximum value: " << statistics.getMaxValue() << std::endl;
+   std::cout << "Simple Moving Average of last " << AVERAGE_QUEUE_SIZE << "values: " << statistics.getAvrValue() << std::endl;
    return 0;
 }
