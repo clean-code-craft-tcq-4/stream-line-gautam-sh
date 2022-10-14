c00d5ebb544bae4762cadb866a0d99dd2c905b48
ygn5cob <38490766+ygn5cob@users.noreply.github.com>
2022-10-14 15:13:57 +0530

    Update receiver.cpp

--- receiver/src/receiver.cpp
+++ receiver/src/receiver.cpp
@@ -5,15 +5,22 @@ int main(int argc, char *argv[])
    double current_value = 0;
    double min_value = 0;
    double max_value = 0;
+   bool is_first_value = true;
    while (std::cin >> current_value)
    {
-      std::cout << current_value << std::endl;
+      std::cout << current_value << std::endl; //this log could be removed later if not needed
+      if(is_first_value)
+      {
+         min_value = current_value;
+         max-value = current_value;
+         is_first_value = false;
+      }
       if(min_value > current_value)
          min_value = current_value;
       if(max_value < current_value)
          max_value = current_value;
    }
-   std::cout << "Minimum data" << min_value << std::endl;
-   std::cout << "Maximum value" << max_value << std::endl;
+   std::cout << "Minimum value: " << min_value << std::endl;
+   std::cout << "Maximum value: " << max_value << std::endl;
    return 0;
 }
