c60aa7f1cf5139e5d3dca9b586f703ad2a9f0000
ygn5cob <38490766+ygn5cob@users.noreply.github.com>
2022-10-14 17:15:26 +0530

    Update statistics.hpp

--- receiver/include/statistics.hpp
+++ receiver/include/statistics.hpp
@@ -8,17 +8,17 @@ const double AVERAGE_QUEUE_SIZE = 5;
 class Statistics
 {
 public:
+  void init(double value);
   void checkMinValue(double value);
   void checkMaxValue(double value);
   void addAvrValue(double value);
   double getMinValue(){return min_value;}
   double getMaxValue(){return max_value;}
-  double getAvrValue(){return avr_value;}
+  double getAvrValue();
 
 private:
   double min_value;
   double max_value;
-  double avr_value;
   std::queue<double> avr_queue;
 };
 
